f6a62c7062db01ef3e1b211e3939105c9b67466c
luna3 <loufouela@alum.us.es>
2025-07-17 21:01:13 +0200

    style: :lipstick: Added search-bar structure (no style yet)

--- movie-electron-app/src/App.jsx
+++ movie-electron-app/src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Movie Searcher</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input className="movie-search-bar" placeholder='Search for your favorite movie!'/>
+          
+          <button onClick={() => setCount((count) => count + 1)}>Search</button>
+          <br></br>
+          You have searched {count} time(s)!
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
