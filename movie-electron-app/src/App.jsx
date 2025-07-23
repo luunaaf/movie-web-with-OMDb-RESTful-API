4525e3d9a9b36bb853db07685c5d684affd03a3c
luna3 <loufouela@alum.us.es>
2025-07-24 00:00:46 +0200

    feat: :sparkles: Post request to OMDP API added

--- movie-electron-app/src/App.jsx
+++ movie-electron-app/src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movie, setMovie] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {movie};
+
+    fetch("http://www.omdbapi.com/?t="+{data}+"&apikey=22770cf6",{
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify(data),
+    }).then(()=>{
+      console.log("i did something hehe")
+    });
+  }
 
   return (
     <>
-      <h1>Movie Searcher</h1>
+      <h1>Movie Finder</h1>
       <div className="card">
-        <input className="movie-search-bar" placeholder='Search for your favorite movie!'/>
-          
-          <button onClick={() => setCount((count) => count + 1)}>Search</button>
-          <br></br>
-          You have searched {count} time(s)!
+        <form onSubmit={handleSubmit}>
+          <input type="text" value = {movie} onChange = {e => setMovie(e.target.value)}className="movie-search-bar" placeholder='Search for your favorite movie!'/>
+            
+            <button onClick={handleSubmit}>Search</button>
+            <br></br>
+            You have searched for the movie {movie}!
+        </form>  
       </div>
     </>
   )
