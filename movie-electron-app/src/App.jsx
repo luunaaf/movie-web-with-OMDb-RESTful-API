724203f2f2432ef8ec2e7dbc0a94d21cbeea8c68
luna3 <loufouela@alum.us.es>
2025-07-24 23:00:02 +0200

    fix: :bug: Fixed bug in api get request

--- movie-electron-app/src/App.jsx
+++ movie-electron-app/src/App.jsx
@@ -3,18 +3,21 @@ import './App.css'
 
 function App() {
   const [movie, setMovie] = useState('')
+  const [title, setTitle] = useState(null);
+  const [isSearched, setIsSearched] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = {movie};
 
-    fetch("http://www.omdbapi.com/?t="+{data}+"&apikey="+ import.meta.env.VITE_OMDB_API_KEY,{
-      mode: 'no-cors',
-      method: 'POST',
-      headers: {"Content-type": "application/json"},
-      body: JSON.stringify(data),
-    }).then(()=>{
-      console.log("i did something hehe")
+    fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${import.meta.env.VITE_OMDB_API_KEY}`,{
+      method: 'GET',
+    })
+    .then((response)=> {return response.json()})
+    .then((search) => {
+      setIsSearched(true);
+      setTitle(search.Title);
+      console.log("This your fav movie?"+search.Title)
     });
   }
 
@@ -24,11 +27,11 @@ function App() {
       <div className="card">
         <form onSubmit={handleSubmit}>
           <input type="text" value = {movie} onChange = {e => setMovie(e.target.value)}className="movie-search-bar" placeholder='Search for your favorite movie!'/>
-            
-            <button onClick={handleSubmit}>Search</button>
-            <br></br>
-            You have searched for the movie {movie}!
-        </form>  
+            <button>Search</button>
+        </form>
+        {isSearched &&
+        <div>{title}
+        </div>}  
       </div>
     </>
   )
